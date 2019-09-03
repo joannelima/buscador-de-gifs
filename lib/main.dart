ffa1122f5fa693f77c6b7e3f2dea9da97915e26f
Allane <allaneregis@gmail.com>
2019-09-03 11:08:13 -0300

    criando o layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:buscador_de_gifs/ui/home_page.dart';
 void main() {
     runApp(MaterialApp(
       home: HomePage(),
+      theme: ThemeData(hintColor: Colors.white),
     ));
 }
 
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,38 +3,83 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  
   String _search;
-  int _offset = 0 ;
+  int _offset = 0;
 
-Future<Map> _getGifs() async{
+  Future<Map> _getGifs() async {
     http.Response response;
-    if(_search == null){
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=2rf5AC2r6mxAltI58EU33hK2WRgzR22R&limit=20&rating=G");
-    }else{
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=2rf5AC2r6mxAltI58EU33hK2WRgzR22R&q=$_search&limit=20&offset=$_offset&rating=G&lang=pt");
+    if (_search == null) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=2rf5AC2r6mxAltI58EU33hK2WRgzR22R&limit=20&rating=G");
+    } else {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=2rf5AC2r6mxAltI58EU33hK2WRgzR22R&q=$_search&limit=20&offset=$_offset&rating=G&lang=pt");
     }
 
     return json.decode(response.body);
+  }
 
-  } 
-  
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
       ),
-      body: Container(),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Pesquise Aqui!",
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
+                textAlign: TextAlign.center,
+              )),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if(snapshot.hasError) return Container();
+                    else return _createGifTable(context, snapshot);
+                }
+              },
+            ),
+          )
+        ],
+      ),
     );
+
   }
+    Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+      return GridView.builder(
+       padding: EdgeInserts.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount , 
+        itemBuilder: null, )
+
+    }
+
 }
